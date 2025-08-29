4d5d9e6a4f4089e14725144479a983bd09c153ea
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2025-08-29 09:45:06 -0300

    update login screen

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   KeyboardAvoidingView,
   Platform,
@@ -12,10 +12,18 @@ import { Button, ButtonText } from "components/ui/button";
 import { useAuthNavigation } from "../../hooks/useTypedNavigation";
 import { VStack } from "components/ui/vstack";
 import { Box } from "components/ui/box";
-import { Input, InputField } from "components/ui/input";
+import { Input, InputField, InputIcon, InputSlot } from "components/ui/input";
+import { AuthHeader } from "./components/headerAuth";
+import { EyeIcon, EyeOffIcon } from "lucide-react-native";
 
 export function LoginScreen() {
   const navigation = useAuthNavigation();
+  const [showPassword, setShowPassword] = useState(false);
+  const handleState = () => {
+    setShowPassword((showState) => {
+      return !showState;
+    });
+  };
 
   const handleLogin = async () => {
     try {
@@ -38,32 +46,41 @@ export function LoginScreen() {
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <VStack className="flex-1 justify-between items-center">
         <Box className="flex-1 justify-center">
-            <Logo width={46} height={46}/>
-            <Text className="text-white font-montserrat-bold text-[30px] mt-6 mb-8 ">Welcome back! Glad to see you, Again!</Text>
+          <AuthHeader>
+            <AuthHeader.Logo />
+            <AuthHeader.Title>Welcome back! Glad to see you, Again!</AuthHeader.Title>
+          </AuthHeader>
 
-            <Box className="gap-4">
-              <Input
-                variant="rounded"
-                className="h-14 bg-[#494358] border-0"
-              >
-                <InputField placeholder="Enter your email" className="font-montserrat-medium text-[13px] text-white"/>
-              </Input>
+          <Box className="gap-4 mt-8">
+            <Input
+              variant="rounded"
+              className="h-14 bg-[#494358] border-0"
+            >
+              <InputField placeholder="Enter your email" className="font-montserrat-medium text-[13px] text-white"/>
+            </Input>
 
-              <Input
-                variant="rounded"
-                className="h-14 bg-[#494358] border-0"
-              >
-                <InputField placeholder="Enter your password" className="font-montserrat-medium text-[13px] text-white"/>
-              </Input>
+            <Input
+              variant="rounded"
+              className="h-14 bg-[#494358] border-0"
+            >
+              <InputField 
+                placeholder="Enter your password" 
+                className="font-montserrat-medium text-[13px] text-white"
+                type={showPassword ? 'text' : 'password'}
+              />
+              <InputSlot className="pr-3" onPress={handleState}>
+                <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
+              </InputSlot>
+            </Input>
 
-              <Button variant="link" className="self-end" onPress={handleNavigatorForgotPassword}>
-                <ButtonText className="font-montserrat-semibold text-xs text-white">Forgot Password?</ButtonText>
-              </Button>
-            </Box>
-
-            <Button className="bg-white rounded-full h-[58px] mt-8" onPress={handleLogin}>
-              <ButtonText className="font-montserrat-semibold text-[#1E232C]">Login</ButtonText>
+            <Button variant="link" className="self-end" onPress={handleNavigatorForgotPassword}>
+              <ButtonText className="font-montserrat-semibold text-xs text-white">Forgot Password?</ButtonText>
             </Button>
+          </Box>
+
+          <Button className="bg-white rounded-full h-[58px] mt-8" onPress={handleLogin}>
+            <ButtonText className="font-montserrat-semibold text-[#1E232C]">Login</ButtonText>
+          </Button>
         </Box>
 
           <Box className=" flex-row items-center gap-1">
